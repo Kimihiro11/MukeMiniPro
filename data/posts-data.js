9105d0826238493ac677404715762b11a41e5889
Kimihiro11 <cjassassinli1125@gmail.com>
2018-07-02 23:28:54 +0800

    post-detail

--- data/posts-data.js
+++ data/posts-data.js
@@ -5,7 +5,8 @@ var local_database = [{
     author_img: "/images/avatar.jpg",
     content: "蒹葭苍苍，白露为霜。所谓伊人，在水一方。溯洄从之，道阻且长。溯游从之，宛在水中央。",
     view_num: "112",
-    collect_num: "78"
+    collect_num: "78",
+    postId:0
   }, {
     date: "Nov 25 2017",
     title: "正是虾肥蟹壮时",
@@ -13,7 +14,8 @@ var local_database = [{
     author_img: "/images/avatar.jpg",
     content: "蒹葭苍苍，白露为霜。所谓伊人，在水一方。溯洄从之，道阻且长。溯游从之，宛在水中央。",
     view_num: "112",
-    collect_num: "78"
+    collect_num: "78",
+    postId: 1
   }, {
     date: "Nov 25 2017",
     title: "正是虾肥蟹壮时",
@@ -21,7 +23,8 @@ var local_database = [{
     author_img: "/images/avatar.jpg",
     content: "蒹葭苍苍，白露为霜。所谓伊人，在水一方。溯洄从之，道阻且长。溯游从之，宛在水中央。",
     view_num: "112",
-    collect_num: "78"
+    collect_num: "78",
+    postId: 2
   }, {
     date: "Nov 25 2017",
     title: "正是虾肥蟹壮时",
@@ -29,7 +32,8 @@ var local_database = [{
     author_img: "/images/avatar.jpg",
     content: "蒹葭苍苍，白露为霜。所谓伊人，在水一方。溯洄从之，道阻且长。溯游从之，宛在水中央。",
     view_num: "112",
-    collect_num: "78"
+    collect_num: "78",
+    postId: 3
   }, {
     date: "Nov 25 2017",
     title: "正是虾肥蟹壮时",
@@ -37,7 +41,8 @@ var local_database = [{
     author_img: "/images/avatar.jpg",
     content: "蒹葭苍苍，白露为霜。所谓伊人，在水一方。溯洄从之，道阻且长。溯游从之，宛在水中央。",
     view_num: "112",
-    collect_num: "78"
+    collect_num: "78",
+    postId: 4
   }
 
 ]
--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,14 +1,11 @@
-var postsData = require('../../data/posts-data.js') 
- 
-       // pages/posts/posts.js
+// pages/posts/post-detail/post-detail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    //小程序总是会读取data对象来做数据绑定，这个动作我们称为动作A
-    //而A的执行，是在onLoad事件执行之后发生的
+  
   },
 
   /**
@@ -16,9 +13,6 @@ Page({
    */
   onLoad: function (options) {
   
-    this.setData({
-      posts_key: postsData.postsList
-    })
   },
 
   /**
@@ -68,13 +62,5 @@ Page({
    */
   onShareAppMessage: function () {
   
-  },
-
-  onPostTap: function (event){
-    var postId = event.currentTarget.dataset.postId;
-    console.log('ssss');
-    wx.navigateTo({
-      url: '../../../post-detail/post-detail',
-    })
   }
 })
