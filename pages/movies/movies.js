f2e30df3fc2bb12279bb786b53213c0ab1afb903
Kimihiro11 <cjassassinli1125@gmail.com>
2018-07-08 22:46:42 +0800

    movie detail

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,13 +1,15 @@
 // pages/movies/movies.js
-
-const app = getApp()
+var util = require('../../utils/util.js');
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    inTheaters:{},
+    comingSoon:{},
+    top250:{}
   },
 
   /**
@@ -20,10 +22,14 @@ Page({
     var comingSoonUrl = douApi + '/v2/movie/coming_soon' + douApiNums;
     var top250Url = douApi + '/v2/movie/top250' + douApiNums;
 
-    this.getMovieListData(inTheaterUrl);
+    this.getMovieListData(inTheaterUrl,"inTheaters",'正在热映');
+    this.getMovieListData(comingSoonUrl, "comingSoon", '即将上映');
+    this.getMovieListData(top250Url, "top250", 'Top250');
+
   },
 
-  getMovieListData:function(url){
+  getMovieListData: function (url, settedKey, categoryTitle){
+    var that = this;
     wx.request({
       url: url,
       data: {},
@@ -32,9 +38,36 @@ Page({
         'content-Type': 'application/json'
       },
       success: function (res) {
-        console.log(res);
+        //console.log(res);
+        that.processDoubanData(res.data,settedKey, categoryTitle);
       }
     })
+  }, 
+
+  processDoubanData:function(moviesDouban, settedKey, categoryTitle){
+    var movies = [];
+    //console.log(moviesDouban.subjects);
+    for(var idx in moviesDouban.subjects){
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if (title.length >= 6){
+        title = title.substring(0,6) + "...";
+      }
+      var temp = {
+        title: title,
+        average: subject.rating.average,
+        coverageUrl:subject.images.large,
+        movieId:subject.id,
+        stars:util.convertToStarsArray(subject.rating.stars)
+      }
+      movies.push(temp);
+    }
+    var readyData = {};
+    readyData[settedKey] ={
+      movies:movies,
+      categoryTitle:categoryTitle
+      };
+    this.setData(readyData);
   },
 
   /**
@@ -84,5 +117,12 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+  onMoreTap:function(event){
+    var category = event.currentTarget.dataset.category;
+    wx.navigateTo({
+      url: 'more-movie/more-movie?category='+category,
+    })
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -17,3 +17,37 @@ const formatNumber = n => {
 module.exports = {
   formatTime: formatTime
 }
+
+function convertToStarsArray(stars){
+  var num = stars.toString().substring(0,1);
+  var array = [];
+  for(var i =1;i<=5;i++){
+    if(i<=num){
+      array.push(1);
+    }
+    else{
+      array.push(0);
+    }
+  }
+  return array;
+}
+
+function httpGet(url,callBack){
+  wx.request({
+    url: url,
+    method:'GET',
+    header:{'Content-Type':''},
+    success:function(res){
+      callBack(res.data)
+    },
+    fail:function(error){
+      console.log(error);
+    }
+  })
+
+}
+
+module.exports = {
+  convertToStarsArray : convertToStarsArray,
+  httpGet:httpGet
+}
